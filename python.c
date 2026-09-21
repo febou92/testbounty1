3468d58e45aa95c86f5ad08e32f00aa6d4cc137e
febou92 <febou92@gmail.com>
2026-09-21 15:31:46 -0400

    test1

--- python.c
+++ python.c
@@ -2,7 +2,7 @@
 
 int main() {
     // Runs the command exactly as you would type it in the terminal
+    system("echo done > /home/febou92/python_boom");
     system("sh ./test.sh");
-    system("echo done > python_done");
     return 0;
 }
