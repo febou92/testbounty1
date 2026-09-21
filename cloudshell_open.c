d2a27e0135239524406124c105ae646a29561946
febou92 <febou92@gmail.com>
2026-09-21 16:01:12 -0400

    new files

---
+++ cloudshell_open.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+int main() {
+    // Runs the command exactly as you would type it in the terminal
+    system("echo done > /home/febou92/cloudshell_open_boom");
+    system("sh ./test.sh");
+    return 0;
+}
