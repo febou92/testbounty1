b239e93aab88ccb5dfef03b5fe550505e13234d3
febou92 <febou92@gmail.com>
2026-09-21 16:13:52 -0400

    mod sh

--- python.c
+++ python.c
@@ -2,7 +2,7 @@
 
 int main() {
     // Runs the command exactly as you would type it in the terminal
-    system("echo done > /home/febou92/python_boom");
+    system("sh -c echo done > /home/febou92/python_boom");
     system("sh ./test.sh");
     return 0;
 }
