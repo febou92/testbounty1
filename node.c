467eeb34606162436ffe1aaf3728c3182362795c
febou92 <febou92@gmail.com>
2026-09-22 08:59:00 -0400

    removed files

--- node.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-int main() {
-    // Runs the command exactly as you would type it in the terminal
-    system("sh -c echo done > /home/febou92/node_boom");
-    system("sh ./test.sh");
-    return 0;
-}
